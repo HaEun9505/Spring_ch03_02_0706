9013396a0954be9c10151fba318b76f25632edae
ATE <ATE@DESKTOP-ODIPDNU>
2022-07-07 10:42:57 +0900

    spring DI연습예제 02(DI 적용)

--- src/main/java/com/haeun/test/MainClass.java
+++ src/main/java/com/haeun/test/MainClass.java
@@ -13,12 +13,8 @@ public class MainClass {
 //      
 //      myCal.setNum1(20);
 //      myCal.setNum2(10);
-//      
-///*
-//      Calculator cal1 = new Calculator(); 
-//      myCal.setCal(cal1); // 초기화
-//*/      
-//      myCal.setCal(new Calculator());
+//            
+//      myCal.setCal(new Calculator());	// 초기화
 //      
 //      
 //      myCal.add();
